93e5d21def94246272e446394bde5bb5f7cbc5cf
LokhandeAniket95 <39650484+LokhandeAniket95@users.noreply.github.com>
2023-11-26 02:06:58 +0530

    Service Registory

---
+++ ServiceRegistory/src/test/java/com/ani/company/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
